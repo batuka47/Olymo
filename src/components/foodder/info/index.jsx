30aba327e93accfcfdcf3b97db977e9feef51ad3
batuka47 <b.batuka0627@gmail.com>
2024-04-29 18:03:13 -0700

    for logo

--- src/components/foodder/info/index.jsx
+++ src/components/foodder/info/index.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import'./style.css'
-
+import Logo from '../../../assets/logo'
 
 
 
@@ -13,12 +13,12 @@ function Info(props){
                 <h1 className='titletext'>Шинэлэг  хурдан тодорхой</h1>
             </div>
             <div className='text'>
-                <div>
+                <div className='con'>
                 <h3 className='tetxtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </h3>
                 <p className='texttext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore int amore ipsum dolor sit amet, consectetur adipiscing elit, sed consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore int amore ipsum et dolore int amore ipsum dolor sit amet, consectetur adipiscing elit, adipiscing elit, sed do eiusmod tempor incididunt ut labore umbre sat et dolore int amore.</p>
                 </div>
-                <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2PRNEIIOi3h2emKqv-tW0W5mobBL2m9KEPwWau3COiA&s" alt="Logo"  width="100%"/>
+                <div className='con2'>
+                <img src="../../../assets/logoz.png" alt="Logo"  width="100%"/>
                 </div>
             </div>
             
